05c19d13b209939e6e672ee8ce8bd8412139f722
John Wiegley <johnw@newartisans.com>
2007-11-04 06:09:10 -0400

    Snapshot 2007-11-04 06:09:08

--- src/ledger-server.lisp
+++ src/ledger-server.lisp
@@ -15,59 +15,59 @@
                                 :default-request-type :post)
     ((journal-path :parameter-type 'string))
   (with-html
-      (:html
-       (:head (:title "Ledger Register"))
-       (:body
-	:style "margin: 50px"
-	(:p (:form
-	     :method :post
-	     (:table
-	      :border 0 :cellpadding 5 :cellspacing 0
-	      (:tr
-	       (:td :style "text-align: right"
-		    (str "Journal path:"))
-	       (:td (:input
-		     :type :text
-		     :style "width: 30em"
-		     :name "journal-path"
-		     :value (or journal-path "sample.dat")))
-	       (:td (:input :type :submit :value "Register"))))))
-	(when journal-path
-	  (let ((binder (binder journal-path)))
-	    (dolist (journal (binder-journals binder))
-	      (htm
-	       (:p
-		(:table
-		 :border 1 :cellpadding 5 :cellspacing 2
-		 :style "margin: 10px 0 0 5%"
-		 (:thead
-		  (:tr (:th "Payee") (:th "Amount") (:th "Total")))
-		 (let ((running-total (integer-to-amount 0))
-		       (zero-amount (integer-to-amount 0)))
-		   (dolist (entry (journal-entries journal))
-		     (dolist (xact (entry-transactions entry))
-		       (let ((amt (xact-amount xact)))
-			 (htm
-			  (:tr
-			   (:td (str (entry-payee entry)))
-			   (:td :style "text-align: right; padding-left: 2em"
-				(print-value (if amt amt zero-amount)))
-			   (:td
-			    :style "text-align: right; padding-left: 2em"
-			    (print-value
-			     (if amt
-				 (setq running-total
-				       (add* running-total amt))
-				 running-total)
-			     :line-feed-string "<br />")))))))
-		   (htm
-		    (:tfoot
-		     (:tr
-		      (:td :colspan 3
-			   :style "text-align: right; padding-left: 2em;
+    (:html
+     (:head (:title "Ledger Register"))
+     (:body
+      :style "margin: 50px"
+      (:p (:form
+	   :method :post
+	   (:table
+	    :border 0 :cellpadding 5 :cellspacing 0
+	    (:tr
+	     (:td :style "text-align: right"
+		  (str "Journal path:"))
+	     (:td (:input
+		   :type :text
+		   :style "width: 30em"
+		   :name "journal-path"
+		   :value (or journal-path "sample.dat")))
+	     (:td (:input :type :submit :value "Register"))))))
+      (when journal-path
+	(let ((binder (binder journal-path)))
+	  (dolist (journal (binder-journals binder))
+	    (htm
+	     (:p
+	      (:table
+	       :border 1 :cellpadding 5 :cellspacing 2
+	       :style "margin: 10px 0 0 5%"
+	       (:thead
+		(:tr (:th "Payee") (:th "Amount") (:th "Total")))
+	       (let ((running-total (integer-to-amount 0))
+		     (zero-amount (integer-to-amount 0)))
+		 (dolist (entry (journal-entries journal))
+		   (dolist (xact (entry-transactions entry))
+		     (let ((amt (xact-amount xact)))
+		       (htm
+			(:tr
+			 (:td (str (entry-payee entry)))
+			 (:td :style "text-align: right; padding-left: 2em"
+			      (print-value (if amt amt zero-amount)))
+			 (:td
+			  :style "text-align: right; padding-left: 2em"
+			  (print-value
+			   (if amt
+			       (setq running-total
+				     (add* running-total amt))
+			       running-total)
+			   :line-feed-string "<br />")))))))
+		 (htm
+		  (:tfoot
+		   (:tr
+		    (:td :colspan 3
+			 :style "text-align: right; padding-left: 2em;
                                  font-weight: bold"
-			   (print-value running-total
-					:line-feed-string "<br />"))))))))))))))))
+			 (print-value running-total
+				      :line-feed-string "<br />"))))))))))))))))
 
 (defun hello-world ()
   (with-html
