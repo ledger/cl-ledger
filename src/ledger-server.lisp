62981ad195338ee88b7f4385c6f92010306d0fab
John Wiegley <johnw@newartisans.com>
2007-11-06 21:31:11 -0400

    Snapshot 2007-11-06 21:31:06

--- src/ledger-server.lisp
+++ src/ledger-server.lisp
@@ -18,45 +18,45 @@
       (:html
        (:head (:title "Ledger Register"))
        (:body
-	:style "margin: 50px"
-	(:p (:form
-	     :method :post
-	     (:table
-	      :border 0 :cellpadding 5 :cellspacing 0
-	      (:tr
-	       (:td :style "text-align: right"
-		    (str "Journal path:"))
-	       (:td (:input
-		     :type :text
-		     :style "width: 30em"
-		     :name "journal-path"
-		     :value (or journal-path "sample.dat")))
-	       (:td (:input :type :submit :value "Register"))))))
-	(when journal-path
-	  (let ((binder (binder journal-path)))
-	    (setq binder (calculate-running-totals
-			  (normalize binder)))
-	    (dolist (journal (binder-journals binder))
-	      (htm
-	       (:p
-		(:table
-		 :border 1 :cellpadding 5 :cellspacing 2
-		 :style "margin: 10px 0 0 5%"
-		 (:thead
-		  (:tr (:th "Payee") (:th "Amount") (:th "Total")))
-		 (dolist (entry (journal-entries journal))
-		   (dolist (xact (entry-transactions entry))
-		     (let ((amt (xact-amount xact)))
-		       (htm
-			(:tr
-			 (:td (str (entry-payee entry)))
-			 (:td :style "text-align: right; padding-left: 2em"
-			      (print-value amt))
-			 (:td
-			  :style "text-align: right; padding-left: 2em"
-			  (print-value
-			   (cdr (assoc :running-total (xact-data xact)))
-			   :line-feed-string "<br />")))))))))))))))))
+        :style "margin: 50px"
+        (:p (:form
+             :method :post
+             (:table
+              :border 0 :cellpadding 5 :cellspacing 0
+              (:tr
+               (:td :style "text-align: right"
+                    (str "Journal path:"))
+               (:td (:input
+                     :type :text
+                     :style "width: 30em"
+                     :name "journal-path"
+                     :value (or journal-path "sample.dat")))
+               (:td (:input :type :submit :value "Register"))))))
+        (when journal-path
+          (let ((binder (binder journal-path)))
+            (setq binder (calculate-running-totals
+                          (normalize binder)))
+            (dolist (journal (binder-journals binder))
+              (htm
+               (:p
+                (:table
+                 :border 1 :cellpadding 5 :cellspacing 2
+                 :style "margin: 10px 0 0 5%"
+                 (:thead
+                  (:tr (:th "Payee") (:th "Amount") (:th "Total")))
+                 (dolist (entry (journal-entries journal))
+                   (dolist (xact (entry-transactions entry))
+                     (let ((amt (xact-amount xact)))
+                       (htm
+                        (:tr
+                         (:td (str (entry-payee entry)))
+                         (:td :style "text-align: right; padding-left: 2em"
+                              (print-value amt))
+                         (:td
+                          :style "text-align: right; padding-left: 2em"
+                          (print-value
+                           (cdr (assoc :running-total (xact-data xact)))
+                           :line-feed-string "<br />")))))))))))))))))
 
 (defun hello-world ()
   (with-html
@@ -66,12 +66,13 @@
       (:h1 "Hello, world!")
       (:p "This is the canonical hello message.")))))
 
-(setq *dispatch-table*
-      (list #'dispatch-easy-handlers
-	    (create-prefix-dispatcher "/hello" #'hello-world)
-	    #'default-dispatcher))
+(progn
+  (setq *dispatch-table*
+        (list #'dispatch-easy-handlers
+              (create-prefix-dispatcher "/hello" #'hello-world)
+              #'default-dispatcher))
 
-(setq *show-lisp-errors-p* t
-      *show-lisp-backtraces-p* t)
+  (setq *show-lisp-errors-p* t
+        *show-lisp-backtraces-p* t))
 
 ;; ledger-server.lisp ends here
