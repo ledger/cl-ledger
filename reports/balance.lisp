b64a938f44ba1b99d4b73eb4563fc00cfb85d6ec
John Wiegley <johnw@newartisans.com>
2007-12-03 11:10:05 -0400

    Balance reports now have 16-char wide amounts

--- reports/balance.lisp
+++ reports/balance.lisp
@@ -19,23 +19,26 @@
     ;; or nil otherwise.  If an account is not printed, its child will see a
     ;; value for "elided" representing how many generations elected not to
     ;; display.
-    (lambda (account real-depth elided &optional finalp)
-      (let ((subtotal (account-value account :subtotal)))
-	(when (or finalp
-		  (or (and subtotal
-			   (not (value-zerop (account-value account :subtotal))))
-		      (> (account-value account :children-with-totals) 1)))
-	  (if (not finalp)
-	      (format output-stream "~12A  ~vA~A~%"
-		      (format-value (account-value account :total) :width 12)
-		      (* (- (1- real-depth) elided) 2) ""
-		      (get-partial-name (account-name account) account
-					elided))
-	      (unless no-total
-		(format output-stream
-			"-----------------------------------------------------~%~12A~%"
-			(format-value (account-value account :total) :width 12))))
-	  t)))))
+    (let ((amounts-width 16))
+     (lambda (account real-depth elided &optional finalp)
+       (let ((subtotal (account-value account :subtotal)))
+	 (when (or finalp
+		   (or (and subtotal
+			    (not (value-zerop (account-value account :subtotal))))
+		       (> (account-value account :children-with-totals) 1)))
+	   (if (not finalp)
+	       (format output-stream "~A  ~vA~A~%"
+		       (format-value (account-value account :total)
+				     :width amounts-width)
+		       (* (- (1- real-depth) elided) 2) ""
+		       (get-partial-name (account-name account) account
+					 elided))
+	       (unless no-total
+		 (format output-stream
+			 "-----------------------------------------------------~%~A~%"
+			 (format-value (account-value account :total)
+				       :width amounts-width))))
+	   t))))))
 
 (defun print-balance (xact-series &key (reporter nil) (no-total nil))
   (let ((root-account (calculate-account-totals xact-series))
@@ -63,7 +66,7 @@
 	(funcall reporter root-account 0 0 t)))))
 
 (defun balance-report (&rest args)
-  (basic-reporter #'print-balance (append (list :balance-report t) args)))
+  (basic-reporter #'print-balance (append args (list :balance-report t))))
 
 (provide 'balance)
 
